9aa05d61cde1b0eddd089f84cc411b74bb2a4ab4
Student <Student@Mac-mini-7.local>
2016-03-10 15:32:47 -0700

    Gold Rush complete with bonus 1, and bonus 3 completed. Just the google maps API to go

--- Goldrush.js
+++ Goldrush.js
@@ -10,20 +10,33 @@
     Gold.pinsArray = []
     Gold.noteInput = "Hi!"
     Gold.okToPlace = true
+    // var img = document.querySelector(".Map")
+    // Gold.imgWidth = img.clientWidth
+    // Gold.imgHeight = img.clientHeight
 
     // a funciton used to get exact coordinates on our map, and pushes a pin
     // into our pinsArray
 
+    function Pin(X,Y){
+      var img = document.querySelector(".Map")
+      this.X = (X / img.clientWidth) *100
+      this.Y = (Y / img.clientHeight) *100
+      this.note = ""
+      this.visible = true
+      this.readNoteVisible = false
+
+      console.log(this.X)
+      console.log(this.Y)
+
+      this.cssMaker = function(){
+        // console.log("top:" +this.Y + "left:"+ this.X)
+        return "top:" +this.Y+"% ;" + "left:"+ this.X + "%;"
+      }
+    }
+
     Gold.getCoordinates = function() {
       if (Gold.okToPlace) {
-        var pin = {
-          X: event.pageX - 14,
-          Y: event.pageY - 47,
-          note:"",
-          visible: true,
-          readNoteVisible: false,
-        }
-        Gold.pinsArray.push(pin)
+        Gold.pinsArray.push(new Pin(event.pageX,event.pageY) )
         console.log(Gold.pinsArray)
         Gold.okToPlace = false
       }
@@ -58,5 +71,11 @@
   }
 }())
 
-
+// var pin = {
+//   X: event.pageX,
+//   Y: event.pageY,
+//   note:"",
+//   visible: true,
+//   readNoteVisible: false,
+// }
 // ['$scope', '$log', GoldDiggerController]
