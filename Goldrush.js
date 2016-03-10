6af14f36646f06ba8065d22e899a6930c4f0d067
Student <Student@Mac-mini-7.local>
2016-03-10 12:30:56 -0700

    The final project complete, with bonus 1 complete as well. Working now on bonus 2

--- Goldrush.js
+++ Goldrush.js
@@ -1,29 +1,56 @@
 ;
 (function() {
   'use strict'
+  // Our Gold Digger Module/Controller
   angular.module('GoldDigger', [])
     .controller("GoldDiggerController", GoldDiggerController)
-
+    // hoisted GoldDiggerController Function
   function GoldDiggerController() {
     var Gold = this
     Gold.pinsArray = []
+    Gold.noteInput = "Hi!"
+    Gold.okToPlace = true
+
+    // a funciton used to get exact coordinates on our map, and pushes a pin
+    // into our pinsArray
+
     Gold.getCoordinates = function() {
-      var pin = {
-        X: event.pageX - 14,
-        Y: event.pageY - 47
+      if (Gold.okToPlace) {
+        var pin = {
+          X: event.pageX - 14,
+          Y: event.pageY - 47,
+          note:"",
+          visible: true,
+          readNoteVisible: false,
+        }
+        Gold.pinsArray.push(pin)
+        console.log(Gold.pinsArray)
+        Gold.okToPlace = false
       }
-      Gold.pinsArray.push(pin)
-      console.log(Gold.pinsArray)
     }
 
+    // a function that removes a pin by erasing that specific pin from
+    // the pinsArray
     Gold.removePin = function(index) {
       Gold.pinsArray.splice(index, 1)
       console.log(index);
     }
 
+    Gold.submitClick = function(index) {
+      Gold.pinsArray[index].note = Gold.noteInput
+      console.log(Gold.pinsArray)
+      Gold.noteInput = ""
+      Gold.pinsArray[index].visible = false
+      Gold.okToPlace = true
+    }
 
+    Gold.hover = function(index){
+      Gold.pinsArray[index].readNoteVisible = true
+    }
 
-
+    Gold.hoverLeave = function(index) {
+      Gold.pinsArray[index].readNoteVisible = false
+    }
 
 
 
